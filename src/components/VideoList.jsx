9bc51b40114ee36f2db3d2c9a0c573e920a1557f
janekbartnicki <jachu01@poczta.onet.pl>
2022-11-06 23:14:40 +0100

    final changes

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 export default function VideoList({videos, onVideoSelect}) {
-    console.log(videos)
     const videosList = videos.map(video => {
         return (
             <div className='ui relaxed divided list' key={video.id.videoId}>
